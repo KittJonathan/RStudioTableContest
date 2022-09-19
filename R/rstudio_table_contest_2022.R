6d1a1126afed6efb345796b4de9b95544da42c0e
Jonathan Kitt <jonathan.kitt@inrae.fr>
2022-09-19 17:09:53 +0200

    update

--- R/rstudio_table_contest_2022.R
+++ R/rstudio_table_contest_2022.R
@@ -5,7 +5,21 @@
 
 library(tidyverse)
 library(gt)
-library(datardis)
+
+# Creating the dataset ----
+
+url <- "https://en.wikipedia.org/wiki/List_of_Apollo_missions"
+
+webpage <- rvest::read_html(url)
+
+tables <- rvest::html_nodes(webpage, "table.wikitable") %>%
+  rvest::html_table(header = TRUE, na.strings = c(NA, ""), convert = TRUE)
+
+d1 <- tables[[6]]
+
+d1 <- d1 %>% 
+  mutate(launch_info = str_split(`Launch date`, "\n"))
+
 
 # Testing {gt} package ----
 
